0848b4543dcd5431125a440920134019a077044e
Abdulrahman Nofal <abdonofa1712@gmail.com>
2023-05-17 15:11:49 +0300

    Changes to be committed: 	modified:   App.js 	new file:   components/ExpensesOutput/ExpensesList.js 	new file:   components/ExpensesOutput/ExpensesOutput.js 	new file:   components/ExpensesOutput/ExpensesSummary.js

--- App.js
+++ App.js
@@ -1,12 +1,11 @@
 import { NavigationContainer } from "@react-navigation/native";
 import { StatusBar } from "expo-status-bar";
-import { StyleSheet, Text, View } from "react-native";
 import { createNativeStackNavigator } from "@react-navigation/native-stack";
 import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
 import ManageExpense from "./screens/ManageExpense";
 import RecentExpenses from "./screens/RecentExpenses";
 import AllExpenses from "./screens/AllExpenses";
-import styles, { GlobalStyles } from "./constants/styles";
+import { GlobalStyles } from "./constants/styles";
 import { Ionicons } from '@expo/vector-icons'; 
 
 const Stack = createNativeStackNavigator();
---
+++ components/ExpensesOutput/ExpensesSummary.js
@@ -0,0 +1,12 @@
+import React from "react";
+import { Text, View } from "react-native";
+const ExpensesSummary = () => {
+  return (
+    <View>
+      <Text>Last 7 Days</Text>
+      <Text>$177.95</Text>
+    </View>
+  );
+};
+
+export default ExpensesSummView;
