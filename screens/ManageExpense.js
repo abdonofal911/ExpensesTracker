e4e588e664561170bd5f441aec6113f473179add
Abdulrahman Nofal <abdonofa1712@gmail.com>
2023-05-17 08:14:21 +0300

    On branch master  Changes to be committed: 	new file:   screens/AllExpenses.js 	new file:   screens/ManageExpense.js 	new file:   screens/RecentExpenses.js
     Changes not staged for commit:
    	modified:   package-lock.json
    	modified:   package.json

---
+++ screens/ManageExpense.js
@@ -0,0 +1,11 @@
+import React from "react";
+import { View, Text } from "react-native";
+
+const ManageExpense = () => {
+  return (
+    <View>
+      <Text>ManageExpense</Text>
+    </View>
+  );
+};
+export default ManageExpense;
