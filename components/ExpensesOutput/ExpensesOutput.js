0d4eb7f126b49c0660321320a6c3c84e87fa80ec
Abdulrahman Nofal <abdonofa1712@gmail.com>
2023-05-18 12:04:13 +0300

    Changes to be committed: 	modified:   components/ExpensesOutput/ExpensesOutput.js
     Changes not staged for commit:
    	modified:   components/ExpensesOutput/ExpensesList.js
    	modified:   components/ExpensesOutput/ExpensesSummary.js

--- components/ExpensesOutput/ExpensesOutput.js
+++ components/ExpensesOutput/ExpensesOutput.js
@@ -1,22 +1,56 @@
 import React from "react";
-import { View, FlatList, Text } from "react-native";
+import { View, FlatList, Text, StyleSheet } from "react-native";
 import ExpensesList from "./ExpensesList";
 import ExpensesSummary from "./ExpensesSummary";
+import { GlobalStyles } from "../../constants/styles";
 
 const Dummy_Expenses = [
-  { id: "e1", description: "a pair of shoes", amount: 29.35 , date:new Date('2021-12-19') },
-  { id: "e2", description: "a Book", amount: 4.25 , date:new Date('2021-6-19')},
-  { id: "e3", description: "a T-Shirt", amount: 14.95 , date:new Date('2021-2-18')},
-  { id: "e4", description: "a Coffe", amount: 0.55 , date:new Date('2021-1-17')},
-  { id: "e5", description: "a Book", amount: 3.55 , date:new Date('2021-3-15')},
+  {
+    id: "e1",
+    description: "a pair of shoes",
+    amount: 29.35,
+    date: new Date("2021-12-19"),
+  },
+  {
+    id: "e2",
+    description: "a Book",
+    amount: 4.25,
+    date: new Date("2021-6-19"),
+  },
+  {
+    id: "e3",
+    description: "a T-Shirt",
+    amount: 14.95,
+    date: new Date("2021-2-18"),
+  },
+  {
+    id: "e4",
+    description: "a Coffe",
+    amount: 0.55,
+    date: new Date("2021-1-17"),
+  },
+  {
+    id: "e5",
+    description: "a Book",
+    amount: 3.55,
+    date: new Date("2021-3-15"),
+  },
 ];
 const ExpensesOutput = ({ expenses, expensesPeriod }) => {
   return (
-    <View>
+    <View style={styles.container}>
       <ExpensesSummary periodName={expensesPeriod} expenses={Dummy_Expenses} />
-      <ExpensesList />
+      <ExpensesList expenses={Dummy_Expenses} />
     </View>
   );
 };
 
 export default ExpensesOutput;
+
+const styles = StyleSheet.create({
+  container: {
+    padding: 24,
+    backgroundColor: GlobalStyles.colors.primary700,
+    flex: 1,
+  },
+});
