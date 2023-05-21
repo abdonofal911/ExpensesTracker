2f26d7bb0903ee65e31f9e6f716771ba8e4bd9b1
Abdulrahman Nofal <abdonofa1712@gmail.com>
2023-05-21 11:02:39 +0300

    Changes to be committed: 	deleted:    components/UI/Button,js 	new file:   components/UI/Button.js 	modified:   screens/ManageExpense.js

--- screens/ManageExpense.js
+++ screens/ManageExpense.js
@@ -2,6 +2,7 @@ import { useLayoutEffect } from "react";
 import { StyleSheet, Text, View } from "react-native";
 import IconButton from "../components/UI/iconButton";
 import { GlobalStyles } from "../constants/styles";
+import Button from "../components/UI/Button.js";
 
 function ManageExpense({ route, navigation }) {
   const editedExpenseId = route.params?.expenseId;
@@ -13,8 +14,19 @@ function ManageExpense({ route, navigation }) {
     });
   }, [navigation, isEditing]);
   const deleteExpenseHanlder = () => {};
+
+  const CancelHanlder = () => {};
+
+  const ConfirmHanlder = () => {};
+
   return (
     <View style={styles.Constainer}>
+      <View style={styles.buttons}>
+        <Button mode="flat" onPress={CancelHanlder} style={styles.button}>
+          Cancel
+        </Button>
+        <Button style={styles.button} onPress={ConfirmHanlder}>{isEditing ? "Update" : "Add"}</Button>
+      </View>
       {isEditing && (
         <View style={styles.deleteContainer}>
           <IconButton
@@ -42,5 +54,14 @@ const styles = StyleSheet.create({
     flex: 1,
     padding: 24,
     backgroundColor: GlobalStyles.colors.primary800,
+  },
+  buttons: {
+    flexDirection: "row",
+    justifyContent: "center",
+    alignItems: "center",
+  },
+  button: {
+    minWidth: 120,
+    marginHorizontal: 8,
   },
 });
