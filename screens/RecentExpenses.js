23f8eaccccc87bc2010c656ac352e977b38a8bc3
Abdulrahman Nofal <abdonofa1712@gmail.com>
2023-05-27 13:38:11 +0300

    Changes to be committed: 	new file:   components/UI/LoadingOverlay.js 	modified:   screens/ManageExpense.js 	modified:   screens/RecentExpenses.js

--- screens/RecentExpenses.js
+++ screens/RecentExpenses.js
@@ -3,20 +3,29 @@ import ExpensesOutput from "../components/ExpensesOutput/ExpensesOutput";
 import { ExpensesContext } from "../store/expenses-context";
 import { getDateMinusDays } from "../util/date";
 import { fetchExpenses } from "../util/http";
+import LoadingOverlay from "../components/UI/LoadingOverlay";
 
 const RecentExpenses = () => {
+  const [isFetching, setIsFetching] = useState();
+
   const expensesCtx = useContext(ExpensesContext);
 
   //  const [fetchedExpenses, setFetchedExpenses] = useState([]);
 
   useEffect(() => {
     async function getExpenses() {
+      setIsFetching(true);
       const expenses = await fetchExpenses();
+      setIsFetching(false);
       // setFetchedExpenses(expenses);
       expensesCtx.setExpenses(expenses);
     }
     getExpenses();
   }, []);
+
+  if (isFetching) { 
+    return <LoadingOverlay />
+  }
 
   const recentExpenses = expensesCtx.expenses.filter((expense) => {
     const today = new Date();
